6e11f25bd76db3edb8a7d42bfde5913f08cffced
Abaad Jubin Abdul Jabbar <abaadjubin@gmail.com>
2023-03-25 18:05:47 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-', ' ':" "}
+morse_text = []
+print("Welcome to the text to Morse Code generator")
+text = input("Enter the text to be converted to Morse Code: ").upper()
+for letter in text:
+    try:
+        morse_text.append(MORSE_CODE_DICT[letter])
+    except KeyError:
+        raise KeyError("Character not present in the Morse Code List")
+morse_code = " ".join(morse_text)
+print(f"The Morse Code for the text is:{morse_code}")
